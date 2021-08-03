4bfb598ac0a9b5f39df1a71eb242a2429eec179c
lucy74310 <lucy74310@gmail.com>
2021-08-04 02:02:44 +0900

    Connection with MongoDB

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://bgjo:test1234!!@boiler-plate.zzxzr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('Mongo DB Connected Successfully!'))
+  .catch(err => console.log(err))
+
+  
 app.get('/', (req, res) => {
   res.send('Hello World!~~안녕하세요~')
 })
